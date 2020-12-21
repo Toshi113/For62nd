14bd8573670e6f961e65a213a1de087896893f72
Toshi113 <yakot4594@gmail.com>
2020-12-21 21:02:29 +0900

    Initial commit

---
+++ app/src/main/java/com/example/for62nd/MemoDatabaseOpenHelper.kt
@@ -0,0 +1,7 @@
+package com.example.for62nd
+
+import android.content.Context
+import android.database.sqlite.SQLiteDatabase
+
+class MemoDatabaseOpenHelper(context: Context,factory: SQLiteDatabase.CursorFactory) {
+}
